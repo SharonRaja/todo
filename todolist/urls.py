41bcaa67a20a82b300a2b9a9efc0aec55cd37c5a
Sharon Raja <sharonrajad@gmail.com>
2025-04-24 18:50:56 +0530

    collected the static file using python manage.py collectstatic

---
+++ todolist/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'todolist'
+
+urlpatterns = [
+    # path("", views.post_list, name='list'),
+    # path("new-post/", views.new_post, name='new_post'),
+    # path("<slug:post_slug>", views.post_page, name='page')
+    path("", views.list_todo)
+]
