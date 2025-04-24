cb66ee5f16dd6ce6b071c65b52afc9626670de47
Sharon Raja <sharonrajad@gmail.com>
2025-04-24 20:46:17 +0530

    basic flow and architecture is set

--- todolist/views.py
+++ todolist/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def list_todo(request):
-    return HttpResponse("todo list page")
+    # return HttpResponse('<h2><b>Home Page, Welcome Home!!!!!!!!!!</b></h2>')
+    return render(request, "list_task.html")
