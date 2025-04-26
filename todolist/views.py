5645f67e370a9800f8399a9ab89f05466883e12c
Sharon Raja <sharonrajad@gmail.com>
2025-04-26 11:29:29 +0530

    add scroling to the list

--- todolist/views.py
+++ todolist/views.py
@@ -3,12 +3,13 @@ from django.http import HttpResponse
 from .models import Task, Tag
 from django.contrib.auth.models import User
 
-print("hello world")
 # Create your views here.
 def list_todo(request):
     # return HttpResponse('<h2><b>Home Page, Welcome Home!!!!!!!!!!</b></h2>')
     print('list_todo')
     tasks = Task.objects.filter(is_done=False)
-    for task in tasks:
-        print(task.task_detail, task.is_done, task.tag_id.tag_name, task.user_id.username, task.created_at)
     return render(request, "list_task.html", {'tasks': tasks})
+
+def add_task(request):
+    print('add_task')
+    return list_todo(request)
